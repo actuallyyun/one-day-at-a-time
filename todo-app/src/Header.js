8fb2f2bbac2798477ab56a3887dda7ba63f1b7df
Yun Ji <this.jiyun@gmail.com>
2022-11-03 09:15:12 +0100

    Create Header component

---
+++ todo-app/src/Header.js
@@ -0,0 +1,20 @@
+
+
+
+const Header = () => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <header>
+                        <h4>Hi,Yun</h4>
+                        <h2>one day at a time.</h2>
+                    </header>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
