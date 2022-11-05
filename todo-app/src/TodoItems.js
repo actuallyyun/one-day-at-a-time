57c74c54546e67ad7df6288e18615d4dc0171931
Yun Ji <this.jiyun@gmail.com>
2022-11-05 08:59:37 +0100

    feature:Done Button delete a todo from todos.But the components are not updated

--- todo-app/src/TodoItems.js
+++ todo-app/src/TodoItems.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 const TodoItems = (props) => {
     return (
@@ -8,9 +8,25 @@ const TodoItems = (props) => {
     )
 }
 
-const DoneToggle = () => {
+const DoneToggle = (props) => {
+
+    console.log("props", props)
+    const [todos, setTodos] = useState(props.todos)
+    const completeTodo = (e) => {
+        console.log(e.target)
+        const todo = props.todo
+        console.log('todo', todo)
+
+
+        console.log("todo before", todos)
+        setTodos(todos.filter(item => item !== todo))
+        console.log("todo afeter", todos)
+    }
+
     return (
-        <button type="button" className="btn btn-success btn-sm">Done</button>
+        <button id={props.id} type="button" className="btn btn-success btn-sm" onClick={(e) => {
+            completeTodo(e)
+        }}>Done</button>
 
     )
 }
@@ -22,12 +38,14 @@ const DeleteToggle = () => {
 }
 
 const ShowTodos = (props) => {
+    const todos = props.todos
     return (
 
-        props.todos.map((todo, index) => {
-            return <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+
+        todos.map((todo, index) => {
+            return <li className="list-group-item d-flex justify-content-between align-items-center" key={index} id={index}>
                 {todo}
-                <DoneToggle />
+                <DoneToggle todos={todos} todo={todo} />
                 <DeleteToggle />
             </li>
         })
